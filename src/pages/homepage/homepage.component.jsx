92bd7c078bbbee134a208ff929f536490fff8d58
Abhishek <sharmaabhishek098@gmail.com>
2020-09-02 23:54:59 +0530

    finshed Styling menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Directroy from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directroy />
+  </div>
+);
+export default HomePage;
