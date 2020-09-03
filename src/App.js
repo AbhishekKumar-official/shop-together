38eabe7b0fe07e9cbc1fea244015f2e56ccfbbde
Abhishek <sharmaabhishek098@gmail.com>
2020-09-03 23:17:57 +0530

    update the menu-item routes using  withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
+  const hatsPage = () => (
+    <div>
+      <h1>Hats page</h1>
+    </div>
+  );
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/hats" component={hatsPage}></Route>
+      </Switch>
     </div>
   );
 }
