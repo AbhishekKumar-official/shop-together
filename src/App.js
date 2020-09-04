c301fb8f853f4a6a58c41cc8ed744b9cef2c289d
Abhishek <sharmaabhishek098@gmail.com>
2020-09-04 19:25:26 +0530

    restarting server bug -1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-import { Switch, Route } from "react-router-dom";
-
+import ShopPage from "./pages/shop/shop.component";
 function App() {
-  const hatsPage = () => (
-    <div>
-      <h1>Hats page</h1>
-    </div>
-  );
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route path="/hats" component={hatsPage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
       </Switch>
     </div>
   );
